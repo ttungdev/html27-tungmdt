8be1b16b638353a663a7f631fc5c26c7018edc8b
tungdev1998 <maiducthanhtung3006@gmail.com>
2022-11-25 14:12:46 +0700

    cap nhat CRUD

--- 2-Examples/Web/js/navbar.js
+++ 2-Examples/Web/js/navbar.js
@@ -1,33 +1,55 @@
 if (typeof document === "undefined") {
-    // during server evaluation
-    console.log("Server loadding...!!!!");
-  } else {
-    //ajax
-    $.get("./header.html", function (data, status) {
-      // alert("Data: " + data + "\nStatus: " + status);
-      document.querySelector("header").innerHTML = data;
-      checkPath() // xu ly bat dong bo
-    });
-  
-    $.get("./footer.html", function (data, status) {
-      // alert("Data: " + data + "\nStatus: " + status);
-      document.querySelector("footer").innerHTML = data;
-    });
-  
-  }
-  
-  function checkPath() {
-    const PATH_NAME = window.location.href;
-    console.log('HREF', PATH_NAME);
-  
-    let js = document.querySelectorAll('a')
-    let listItemNav = $('a') 
-    for (let index = 0; index < listItemNav.length; index++) {
-      const element = listItemNav[index];
-      if (PATH_NAME === element.href) {
-        $(element).addClass( "active text-danger" );
-      }
+  // during server evaluation
+  console.log("Server loadding...!!!!");
+} else {
+  //ajax
+  $.get("./header.html", function (data, status) {
+    // alert("Data: " + data + "\nStatus: " + status);
+    let elm = document.querySelector("header");
+    if (elm) {
+      elm.innerHTML = data;
     }
-    
+    checkPath();
+    checkLogin();
+  });
+
+  $.get("./footer.html", function (data, status) {
+    // alert("Data: " + data + "\nStatus: " + status);
+    let elm = document.querySelector("footer");
+    if (elm) {
+      elm.innerHTML = data;
+    }
+  });
+}
+
+function checkPath() {
+  const PATH_NAME = window.location.href;
+  console.log("HREF", PATH_NAME);
+
+  let js = document.querySelectorAll("a");
+  let listItemNav = $("a");
+  for (let index = 0; index < listItemNav.length; index++) {
+    const element = listItemNav[index];
+    if (PATH_NAME === element.href) {
+      $(element).addClass("active text-danger");
+    }
   }
-  
+}
+function checkLogin() {
+  let url = new URL(window.location.href);
+  let isLogin = url.searchParams.get("islogin");
+  let btnLogin = document.getElementById("btn__login");
+  let btnLogout = document.getElementById("btn__logout");
+  if (isLogin == 1) {
+    btnLogin.classList.add("d-none");
+    btnLogin.classList.remove("d-block");
+    btnLogout.classList.add("d-block");
+    btnLogout.classList.remove("d-none");
+  } else {
+    btnLogout.classList.add("d-none");
+    btnLogout.classList.remove("d-block");
+    btnLogin.classList.add("d-block");
+    btnLogin.classList.remove("d-none");
+  }
+  console.log(isLogin);
+}
